636fcfaa00e61da0a6a7eecd74e3761c7bcfd461
kuhlen <kuhlen@physik.rwth-aachen.de>
2016-05-17 17:57:42 +0200

    Bugfixes: file extension not recognized if there is an additional dot, old buffer name restriction may prefent file from loading.

--- app/src/main/java/de/rwth_aachen/phyphox/phyphoxFile.java
+++ app/src/main/java/de/rwth_aachen/phyphox/phyphoxFile.java
@@ -56,14 +56,14 @@ public abstract class phyphoxFile {
         if (s.isEmpty()) {
             return false;
         }
-        if (!Character.isJavaIdentifierStart(s.charAt(0))) {
-            return false;
-        }
-        for (int i = 1; i < s.length(); i++) {
-            if (!Character.isJavaIdentifierPart(s.charAt(i))) {
-                return false;
-            }
-        }
+//        if (!Character.isJavaIdentifierStart(s.charAt(0))) {
+//            return false;
+//        }
+//        for (int i = 1; i < s.length(); i++) {
+//            if (!Character.isJavaIdentifierPart(s.charAt(i))) {
+//                return false;
+//            }
+//        }
         return true;
     }
 
