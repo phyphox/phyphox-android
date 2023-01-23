bfcf25e68efa51d58b8918f282c99188b01dbb8a
GTripathee <tripathee.gaurav@gmail.com>
2023-01-23 15:14:50 +0100

    ui: make setting page support light mode

--- app/src/main/java/de/rwth_aachen/phyphox/Settings.java
+++ app/src/main/java/de/rwth_aachen/phyphox/Settings.java
@@ -11,7 +11,6 @@ public class Settings extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(R.style.experimentList);
         setContentView(R.layout.activity_settings);
 
         SettingsFragment settingsFragment = new SettingsFragment();
