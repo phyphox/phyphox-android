e0b5f362c26844eb6526de8cf2ad8dcd872972f3
Staacks <staacks@physik.rwth-aachen.de>
2025-06-06 22:12:17 +0200

    Fix experiment activity not restarting after permission change.

--- app/src/main/java/de/rwth_aachen/phyphox/Experiment.java
+++ app/src/main/java/de/rwth_aachen/phyphox/Experiment.java
@@ -384,7 +384,7 @@ public class Experiment extends AppCompatActivity implements View.OnClickListene
     //   with the formerly missing permission
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode != REQUEST_WRITE_EXTERNAL_STORAGE){
+        if (requestCode == REQUEST_WRITE_EXTERNAL_STORAGE){
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Permission granted. You can now save the file.", Toast.LENGTH_SHORT).show();
             } else {
