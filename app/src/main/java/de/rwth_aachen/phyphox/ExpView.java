3e2d178823762ed3d7208033fb53cbfada7a74b0
Staacks <sebastian@staacks.de>
2026-05-30 21:51:40 +0200

    Fix visibility parameter for empty data containers.

--- app/src/main/java/de/rwth_aachen/phyphox/ExpView.java
+++ app/src/main/java/de/rwth_aachen/phyphox/ExpView.java
@@ -343,8 +343,8 @@ public class ExpView implements Serializable{
         }
 
         private void updateViewElementVisibility(){
-            if(visibilityBuffer != null){
-                if(visibilityBuffer.value <= 0 || visibilityBuffer.size == 0){
+            if (visibilityBuffer != null) {
+                if (Double.isNaN(visibilityBuffer.value) || visibilityBuffer.value <= 0) {
                     if (state == State.maximized) {
                         //This prevents from leaving the user with an entirely empty UI, when an element might be maximized while it becomes hidden.
                         restore();
